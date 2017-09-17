e24768caf6af8022497f2b77cac94059a0ed4638
Samiullah Khan <samiullah.khan.1989@gmail.com>
2017-09-17 18:04:50 +0500

    the translation is added for add item

--- src/pages/add-shopping-list/add-shopping-list.module.ts
+++ src/pages/add-shopping-list/add-shopping-list.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AddShoppingListPage } from './add-shopping-list';
 
+import { TranslateModule } from "@ngx-translate/core";
+
 @NgModule({
   declarations: [
     AddShoppingListPage,
   ],
   imports: [
+    TranslateModule.forChild(),
     IonicPageModule.forChild(AddShoppingListPage),
   ],
 })
